2325a53afd591cbc264449385603c06635dc4f6d
eduardochvzb <eduardochvzb@gmail.com>
2020-06-17 18:31:31 -0500

    completo

--- main.js
+++ main.js
@@ -1,93 +1,162 @@
-import getData from './getData.js'
+import getData from "./getData.js";
 
-const url = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json"
+const url =
+  "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json";
 
-const render = data => {
-    const title = 'United States GDP'
-    const svg = d3.select('svg');
-    const height = svg.attr('height');
-    const width = svg.attr('width');
-    const margin = {
-        left: 110,
-        right: 30,
-        top: 100,
-        bottom: 90
-    };
-    const inner={
-        height: height -(margin.top+margin.bottom),
-        width: width -(margin.left+margin.right)
-    }
-    const dataSet = data.data;
+const render = (data) => {
+  const title = "Doping in Professional Bicycle Racing";
+  const labelAxisX = "";
+  const labelAxisY = "Time in Minutes";
+  const svg = d3.select("svg");
+  const height = svg.attr("height");
+  const width = svg.attr("width");
+  const margin = {
+    left: 110,
+    right: 30,
+    top: 80,
+    bottom: 100,
+  };
+  const padding = {
+    left: 15,
+    bottom: 10,
+  };
+  const pointRadius = 7;
+  const inner = {
+    height: height - (margin.top + margin.bottom),
+    width: width - (margin.left + margin.right),
+  };
+  const dataSet = data;
 
-    const xValue = d => d[0], yValue = d => d[1];
+  const xValue = (d) => d.Year,
+    yValue = (d) => d.Time;
+  const changeColor = (d) => {
+    if (d.Doping) return "doping point";
+    return "no-doping point";
+  };
+  const tooltip = (d) => {
+    return (
+      d.Name +
+      ": " +
+      d.Nationality +
+      "<br>Year: " +
+      d3.timeFormat("%Y")(d.Year) +
+      ", Time: " +
+      d3.timeFormat("%M:%S")(d.Time) +
+      "<br>" +
+      d.Doping
+    );
+  };
+  const xScale = d3
+    .scaleTime()
+    .domain(d3.extent(dataSet, (d) => xValue(d)))
+    .range([0, inner.width]);
+  const yScale = d3
+    .scaleTime()
+    .domain(d3.extent(dataSet, (d) => yValue(d)))
+    .range([0, inner.height]);
+  const g = svg
+    .append("g")
+    .attr("transform", `translate(${margin.left},${margin.top})`);
 
-    const maxValue = d3.max(dataSet, d => yValue(d));
+  const xAxis = d3
+    .axisBottom(xScale)
+    .tickSize(-(inner.height + padding.bottom))
+    .tickFormat(d3.timeFormat("%Y"));
+  const yAxis = d3
+    .axisLeft(yScale)
+    .tickFormat(d3.timeFormat("%M:%S"))
+    .tickSize(-(inner.width + padding.left));
 
-    const xScale = d3.scaleTime()
-        .domain([d3.min(dataSet, d=> xValue(d)),d3.max(dataSet, d=> xValue(d))])
-        .range([0,inner.width])
-    const yScale = d3.scaleLinear()
-        .domain([0,maxValue])
-        .range([inner.height,0]);
-    const g = svg.append('g')
-        .attr('transform', `translate(${margin.left},${margin.top})`);
+  g.append("g")
+    .call(yAxis)
+    .attr("transform", `translate(${-padding.left},0)`)
+    .append("text")
+    .text(labelAxisY)
+    .attr("fill", "#000")
+    .attr("y", -50)
+    .attr("x", 70 - inner.height / 2)
+    .attr("transform", "rotate(270)")
+    .attr("class", "info");
+  g.append("g")
+    .call(xAxis)
+    .attr("transform", `translate(0,${inner.height + padding.bottom})`);
 
-    const xAxis = d3.axisBottom(xScale)
-        .tickFormat(d3.timeFormat("%Y"));
-    const yAxis = d3.axisLeft(yScale)
-        .tickSize(-inner.width);
+  const info = g.append("g");
+  const infoColorSize = 12;
+  const offsetInfo = 50;
+  const marginInfoText = 14;
+  info
+    .append("rect")
+    .attr("class", "no-doping")
+    .attr("width", infoColorSize)
+    .attr("transform", `translate(0,${inner.height + padding.bottom + offsetInfo-infoColorSize})`)
+    .attr("height", infoColorSize);
+  info
+    .append("text")
+    .attr("transform", `translate(${marginInfoText},${inner.height + padding.bottom + offsetInfo})`)
+    .text("No doping allegations")
+    .attr("class", "info");
+  info
+    .append("rect")
+    .attr("class", "doping")
+    .attr("width", infoColorSize)
+    .attr("height", infoColorSize)
+    .attr(
+      "transform",
+      `translate(${inner.width / 2},${inner.height + padding.bottom + offsetInfo -infoColorSize})`
+    );
+  info
+    .append("text")
+    .attr(
+      "transform",
+      `translate(${inner.width / 2 + marginInfoText},${inner.height + padding.bottom + offsetInfo})`
+    )
+    .attr("class", "info")
+    .text("Riders with doping allegations");
 
-    g.append('g').call(yAxis).append('text')
-        .text('Gross Domestic Product')
-        .attr('fill','#000')
-        .attr('y',-70)
-        .attr('x', 80-inner.height/2)
-        .attr('transform','rotate(270)')
-        .attr('class','info');
-    g.append('g').call(xAxis)
-        .attr('transform', `translate(0,${inner.height})`).append('text')
-            .text('More Information: http://www.bea.gov/national/pdf/nipaguid.pdf')
-            .attr('fill','black')
-            .attr('x',inner.width-200)
-            .attr('y',60)
-            .attr('class','info');
+  const div = d3
+    .select("body")
+    .append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
 
-    const div = d3.select("body").append("div")	
-        .attr("class", "tooltip")				
-        .style("opacity", 0);
-
-    g.selectAll('rect').data(dataSet)
-        .enter().append('rect')
-            .attr('x', d => xScale(xValue(d)))
-            .attr('width', inner.width/dataSet.length)
-            .attr('height', d => inner.height-yScale(yValue(d)))
-            .attr('y', d => yScale(yValue(d)))
-            .attr('class', 'bar')
-            .on('mouseover', d=>{
-                div.transition()
-                    .duration(200)
-                    .style('opacity', 0.9);
-                div.html('<strong>'+d3.timeFormat('%Y')(xValue(d))+'</strong><br>$'+yValue(d).toLocaleString('en')+' Billion')
-                    .style('left', (d3.event.pageX)+'px')
-                    .style('top', (d3.event.pageY)+'px');
-            })
-            .on('mouseout', d=>{
-                div.transition()
-                    .duration(500)
-                    .style('opacity',0)
-            })
-    
-    svg.append('text')
-        .text(title)
-        .attr('class','title')
-        .attr('x', 44)
-        .attr('y', 50);
-}
-const dateParse = d3.timeParse('%Y-%m-%d');
-getData(url).then(data =>{
-    data.data.forEach(d =>{
-        d[0] = dateParse(d[0])
-        d[1] = +d[1];
+  g.selectAll("circle")
+    .data(dataSet)
+    .enter()
+    .append("circle")
+    .attr("cx", (d) => xScale(xValue(d)))
+    .attr("r", pointRadius)
+    .attr("cy", (d) => yScale(yValue(d)))
+    .attr("class", (d) => changeColor(d))
+    .on("mouseover", (d) => {
+      div.transition().duration(200).style("opacity", 0.9);
+      div
+        .html(tooltip(d))
+        .style("left", d3.event.pageX + "px")
+        .style("top", d3.event.pageY + "px");
     })
+    .on("mouseout", (d) => {
+      div.transition().duration(500).style("opacity", 0);
+    });
+
+  svg
+    .append("text")
+    .text(title)
+    .attr("class", "title")
+    .attr("x", 44)
+    .attr("y", 50);
+};
+const dateParseYear = d3.timeParse("%Y");
+const dateParseTime = d3.timeParse("%M:%S");
+getData(url)
+  .then((data) => {
+    data.forEach((d) => {
+      d.Place = +d.Place;
+      d.Seconds = +d.Seconds;
+      d.Year = dateParseYear(d.Year);
+      d.Time = dateParseTime(d.Time);
+    });
+    console.log(data);
     render(data);
-}).catch(err => console.error(err));
+  })
+  .catch((err) => console.error(err));
